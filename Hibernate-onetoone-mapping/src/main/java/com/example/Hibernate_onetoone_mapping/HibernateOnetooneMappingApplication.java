afac21ebabeec28d7370cbef61188ed8ab6f0182
dharshan2003 <112673415+dharshan2003@users.noreply.github.com>
2025-12-03 11:06:38 +0530

    Add files via upload

---
+++ Hibernate-onetoone-mapping/src/main/java/com/example/Hibernate_onetoone_mapping/HibernateOnetooneMappingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hibernate_onetoone_mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateOnetooneMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateOnetooneMappingApplication.class, args);
+	}
+
+}
---
+++ Hibernate-onetoone-mapping/src/test/java/com/example/Hibernate_onetoone_mapping/HibernateOnetooneMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hibernate_onetoone_mapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateOnetooneMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
